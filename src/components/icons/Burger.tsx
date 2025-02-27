28b0ac973c1905e39b74efdb7882a33d64d01fdd
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2025-02-27 09:59:36 -0300

    Add icons used in pages of the app

---
+++ src/components/icons/Burger.tsx
@@ -0,0 +1,22 @@
+export default function Burger({ className }: { className: string }) {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="33"
+        height="33"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className={className}
+      >
+        <line x1="4" x2="20" y1="12" y2="12" />
+        <line x1="4" x2="20" y1="6" y2="6" />
+        <line x1="4" x2="20" y1="18" y2="18" />
+      </svg>
+    </>
+  );
+}
