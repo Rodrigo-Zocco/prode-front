accbc7db39382fd1c88b5dd2c9c09777dfc162e1
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2025-02-28 11:08:35 -0300

    Refactor league page to do the fetch within a suspense

--- src/app/ligas/[leagueId]/page.tsx
+++ src/app/ligas/[leagueId]/page.tsx
@@ -1,7 +1,6 @@
-import LeagueContainer from "@/components/league-container";
-import Header from "@/components/league-container/header";
-import Matches from "@/components/league-container/matches";
-import Results from "@/components/league-container/results";
+import LeagueLayout from "@/components/league-layout";
+import Loading from "@/components/ui/loading";
+import { Suspense } from "react";
 
 export default async function Liga({
   params,
@@ -9,17 +8,11 @@ export default async function Liga({
   params: Promise<{ leagueId: string }>;
 }) {
   const leagueId = (await params).leagueId;
-  await new Promise(r => setTimeout(r, 3000));
   return (
-    <>
-      <LeagueContainer>
-        <Header
-          leagueName={"Superliga Argentina"}
-          logoUrl={"x"}
-        />
-        <Matches />
-        <Results />
-      </LeagueContainer>
-    </>
+    <main>
+      <Suspense fallback={<Loading />}>
+        <LeagueLayout leagueId={leagueId} />
+      </Suspense>
+    </main>
   );
 }
